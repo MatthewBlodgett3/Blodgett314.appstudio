14b7ce473a0eb92bd33cbf9818f07776ca13b829
Matthew Blodgett <matthewblodgett@creighton.edu>
2021-03-16 20:57:29 -0500

    Blodgett314

---
+++ Blodgett314.appstudio/forms/FunctionCompare/FunctionCompare.js
@@ -0,0 +1,20 @@
+/*
+
+function comparePassword(password1, password2){
+    if(password1 == password2) {
+    return true
+} else {
+    return false
+    }
+    }
+    
+    pass1 = prompt('Enter your new password.')
+    pass2 = prompt('Enter your new password.')
+    result = comparePassword(pass1, pass2)
+    if (result == true) {
+    console.log('The passwords are the same.')
+}else {
+    console.log('The passwords are not the same')
+    }
+    
+*/
---
+++ Blodgett314.appstudio/forms/cityWhile/cityWhile.js
@@ -0,0 +1,31 @@
+/*
+let cities = []
+let i = 0
+let doneWithCities = false
+while (doneWithCities == false){
+    cities[i] = prompt("Enter the next City: ")
+    i++
+    doneWithCities = confirm("Are you done with the Cities?")
+    }
+console.log(cities)
+*/
+/*
+var cities = []
+let doneWithCities = true
+
+while(doneWithCities==true){
+    city = prompt("Enter the next City(type 'stop' to finish your list)")
+    city = city.toString().toLowerCase()
+    if(city == 'stop'){
+    doneWithCities = false
+}else{
+    cities.push(city)
+    }
+}
+var i = 0
+while(cities[i]){
+    console.log(cities[i])
+    i++
+    }
+
+*/
